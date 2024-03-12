0e357309bc518239ebb6739b1bccf640e3a977fa
Stanwade <923926049@qq.com>
2024-03-12 19:49:50 +0800

    add ssim calc

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import torch
 from inspect import isfunction
 from torch.utils.data import DataLoader
 from datasets import MNISTDataset, MMFDataset, MMFGrayScaleDataset, MMFMNISTDataset
@@ -10,6 +11,7 @@ def plot_imgs(inputs,name:str, dir:str='imgs', figsize = (16,16)):
         axes[idx].imshow(inputs[idx].squeeze().numpy(), cmap='gray')
         axes[idx].set_xticks([])
         axes[idx].set_yticks([])
+        axes[idx].text(0, 0, f'{name}_{idx}')
     plt.tight_layout()
     plt.savefig(f'{dir}/{name}.png')
     
@@ -74,3 +76,32 @@ def create_dataloader(dataset_type: str,
         return train_dataset, validation_dataset, train_loader, validation_loader
     else:
         return train_loader, validation_loader
+    
+
+def calculate_ssim(x, y, k1 = 0.01, k2 = 0.03, L = 255):
+    
+    if not isinstance(x, torch.Tensor):
+        x = torch.tensor(x)
+    elif not isinstance(y, torch.Tensor):
+        y = torch.tensor(y)
+    
+    assert x.shape == y.shape, f'input tensors must have the same shape, x shape is {x.shape}, y shape is {y.shape}'
+    
+    C1 = (k1 * L) ** 2
+    C2 = (k2 * L) ** 2
+
+    mu_x = x.mean(dim=[1, 2, 3], keepdim=True)
+    mu_y = y.mean(dim=[1, 2, 3], keepdim=True)
+    mu_x_mu_y = mu_x * mu_y
+    sigma_x_sq = x.var(dim=[1, 2, 3], unbiased=False, keepdim=True)
+    sigma_y_sq = y.var(dim=[1, 2, 3], unbiased=False, keepdim=True)
+    sigma_x_y = torch.mean(x * y, dim=[1, 2, 3], keepdim=True) - mu_x_mu_y
+    
+    mu_x_sq = mu_x.pow(2)
+    mu_y_sq = mu_y.pow(2)
+
+    l = (2 * mu_x_mu_y + C1) / (mu_x_sq + mu_y_sq + C1)
+    c = (2 * sigma_x_y + C2) / (sigma_x_sq + sigma_y_sq + C2)
+
+    return l * c
+
