55e480ee549b3102a132fe9f5412873d5057c0a0
Stanwade <923926049@qq.com>
2024-04-27 08:05:15 +0000

    add UNetModel

---
+++ UNetModel/model.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn.functional as F
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from torchvision import transforms
+
+from ReconstructionModel.networks import FullyConnectedNetwork
+from torchvision.utils import make_grid
+
+class ReconstructionModel(pl.LightningModule):
+    def __init__(self, in_img_shape: torch.Tensor,
+                 out_img_shape: torch.Tensor,
+                 mid_lengths: list,
+                 norm_type:str,
+                 activation:str,
+                 img_size:int = 16,
+                 **kwargs):
+        super().__init__()
+        self.save_hyperparameters()
+        self.model = FullyConnectedNetwork(in_img_shape,
+                                           out_img_shape,
+                                           mid_lengths=mid_lengths,
+                                           norm_type=norm_type,
+                                           activation=activation,
+                                           img_size = img_size,
+                                           **kwargs)
+        self.input_size = in_img_shape
+        # self.logger:TensorBoardLogger = logger
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log('train_loss', loss, sync_dist=True)
+        
+        lr = self.trainer.optimizers[0].param_groups[0]['lr']
+        self.log('learning rate', lr, on_step=True, sync_dist=True)
+        
+        with torch.no_grad():
+            if batch_idx % 100 == 0:
+                num_samples = 3
+                resize = transforms.Resize(size=(100,100),interpolation=transforms.InterpolationMode.NEAREST)
+                
+                x = x[:num_samples]
+                y = y[:num_samples]
+                y_hat = y_hat[:num_samples]
+                
+                x = resize(x) / torch.max(x)
+                y = resize(y) / torch.max(y)
+                y_hat = resize(y_hat) / torch.max(y_hat)
+                
+                combine = torch.cat((x,y,y_hat), dim=0)
+                
+                grid = make_grid(combine, nrow=num_samples)
+                
+                self.logger.experiment.add_image('img_log',grid,self.global_step)
+        
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log('val_loss', loss, sync_dist=True)
+        
+        # outputs = {'input' : x,
+        #            'output' : y_hat,
+        #            'label' : y,
+        #            'loss' : loss}
+        return loss
+    
+    def on_train_epoch_end(self) -> None:
+        if self.current_epoch == 0:
+            sampleImg=torch.randn(self.input_size).unsqueeze(0).to('cuda')
+            self.logger.experiment.add_graph(self.model,sampleImg)
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters())
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, mode='min', factor=0.1, patience=5
+        )
+        return {
+                'optimizer': optimizer,
+                'lr_scheduler': {
+                    'scheduler': scheduler,
+                    'monitor': 'val_loss'
+                    }
+                }
---
+++ UNetModel/train.py
@@ -0,0 +1,67 @@
+from ReconstructionModel.model import ReconstructionModel
+
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from datasets import create_dataloader
+
+from typing import Optional
+
+def train_reconstruction_model(reconstruction_model,
+                               train_loader,
+                               validation_loader,
+                               logger,
+                               num_epochs=100,
+                               callbacks=[]):
+    
+    trainer = Trainer(max_epochs=num_epochs,
+                      callbacks=callbacks,
+                      gradient_clip_val=0.6,
+                      logger=logger,
+                      devices='0,1,2,3')
+    reconstruction_model_trainer = reconstruction_model
+    trainer.fit(reconstruction_model_trainer,
+                train_dataloaders=train_loader,
+                val_dataloaders=validation_loader)
+    
+if __name__ == '__main__':
+    
+    pl.seed_everything(42)
+    logger = TensorBoardLogger(save_dir='./',log_graph=True)
+    
+    model_checkpoint_callback = ModelCheckpoint(
+        monitor='val_loss',
+        filename='{epoch}-{val_loss:.4f}',
+        dirpath='ReconstructionModel/ckpts_imglogger_test',
+        mode='min',
+        every_n_epochs=10,
+        save_top_k=3,
+        save_last=True
+    )
+    
+    dataset_type = 'MMFFMNIST_GRAY'
+    label_size = 32
+    
+    train_dataset, valid_dataset, train_loader, valid_loader = create_dataloader(dataset_type,need_datasets=True)
+    
+    reconstruction_model = ReconstructionModel(in_img_shape=train_dataset[0][0].shape,
+                                               out_img_shape=train_dataset[0][1].shape,
+                                               mid_lengths=[],
+                                               norm_type='batchnorm',
+                                               img_size=label_size,
+                                               activation='lrelu')
+    
+    train_reconstruction_model(reconstruction_model,
+                               train_loader,
+                               valid_loader,
+                               logger=logger,
+                               num_epochs = 10,
+                               callbacks = [model_checkpoint_callback])
